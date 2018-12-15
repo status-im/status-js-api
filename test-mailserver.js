22c72bf45d00cf95a8eb30b4106618c4ca13b2cb
Richard Ramos <info@richardramos.me>
2018-12-15 00:12:13 -0400

    Updated mailserver test

--- test-mailserver.js
+++ test-mailserver.js
@@ -2,7 +2,8 @@ var StatusJS = require('./dist/index.js');
 
 (async () => {
 var status = new StatusJS();
-  await status.connect("/home/richard/.statusd/geth.ipc");
+  await status.connect("ws://localhost:8546");
+ // await status.connect("/home/richard/.statusd/geth.ipc");
 
   const channel = "mytest";
 
@@ -13,7 +14,11 @@ var status = new StatusJS();
   });
 
   status.mailservers.useMailserver("mail-02.gc-us-central1-a.eth.beta", (err, res) => {
-    status.mailservers.requestMessages(channel, {}, (err, res) => { 
+
+    const from = 1544783388; // unix timestamp
+    const to = 1544846928;
+
+    status.mailservers.requestMessages(channel, {from, to}, (err, res) => { 
       if(err) console.log(err); 
     });
   });
